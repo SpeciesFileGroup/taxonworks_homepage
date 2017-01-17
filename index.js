120b20106eb4c760276ec3776783741a2d163050
BDub & Mac <brianw@pixotech.com>
2017-01-17 11:32:17 -0600

    Added YAML config for the homepage. Added YAMLjs.

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const nunjucks = require("nunjucks");
 const stylus = require("stylus");
 const fs = require("fs");
 const del = require("del");
+const yamljs = require("yamljs");
+
+const homepageContext = yamljs.load("config/homepage.yaml");
 
 const nunjucksEnvironment = new nunjucks.Environment(new nunjucks.FileSystemLoader('templates'));
-const homepage = nunjucksEnvironment.render("pages/homepage.njk");
+const homepage = nunjucksEnvironment.render("pages/homepage.njk", homepageContext);
 
 del.sync(["build/**"]);
 fs.mkdirSync("build");
