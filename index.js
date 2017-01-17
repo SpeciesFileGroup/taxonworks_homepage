f984fe01cedeb2728ccdf67795c9cf11c4343892
BDub & Mac <brianw@pixotech.com>
2017-01-17 11:54:04 -0600

    added stylus to build pipeline

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const fs = require("fs");
 const del = require("del");
 const yamljs = require("yamljs");
 
+del.sync(["build/**"]);
+const nunjucksEnvironment = new nunjucks.Environment(new nunjucks.FileSystemLoader('templates'));
+
 const homepageContext = yamljs.load("config/homepage.yaml");
 
-const nunjucksEnvironment = new nunjucks.Environment(new nunjucks.FileSystemLoader('templates'));
 const homepage = nunjucksEnvironment.render("pages/homepage.njk", homepageContext);
+const homepageStyle = fs.readFileSync("templates/pages/homepage.styl", "UTF-8");
+const homepageCSS = stylus(homepageStyle).render();
 
-del.sync(["build/**"]);
 fs.mkdirSync("build");
 fs.writeFileSync("build/index.html", homepage, {encoding: "UTF-8"});
+fs.writeFileSync("build/homepage.css", homepageCSS, {encoding: "UTF-8"});
