de9239df6f3d5b837fa4306c9727997e2045479a
Aaron McNeal <aaron@pixotech.com>
2017-04-04 11:10:09 -0500

    Updates

--- bundle.js
+++ bundle.js
@@ -637,6 +637,27 @@
   return animation;
 
 }));
+
 document.addEventListener("DOMContentLoaded", function() {
 
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var twHeader = document.querySelector('header');
+    var twNavBar = document.querySelector('.header__nav-bar');
+    var twMain   = document.querySelector('main');
+
+    twNavBar.style.visibility = 'hidden';
+
+    window.onscroll = function() {
+        var scrollPos = document.getElementsByTagName("body")[0].scrollTop;
+        if ( scrollPos > twMain.offsetTop )
+        {
+            twNavBar.style.visibility = 'visible';
+        } else {
+            twNavBar.style.visibility = 'hidden';
+        }
+    };
+});
+
