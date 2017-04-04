a080e6a458522bb0dbef817de185b5c02243c0d5
Aaron McNeal <aaron@pixotech.com>
2017-04-04 11:09:52 -0500

    Script formatting. Added scroll event listener for displaying nav bar. Default nav bar style is hidden.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,7 +1,19 @@
-(function ($) {
-    $(document).ready(function() {
-        $(".header__nav-bar").hide();
 
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    var twHeader = document.querySelector('header');
+    var twNavBar = document.querySelector('.header__nav-bar');
+    var twMain   = document.querySelector('main');
 
+    twNavBar.style.visibility = 'hidden';
+
+    window.onscroll = function() {
+        var scrollPos = document.getElementsByTagName("body")[0].scrollTop;
+        if ( scrollPos > twMain.offsetTop )
+        {
+            twNavBar.style.visibility = 'visible';
+        } else {
+            twNavBar.style.visibility = 'hidden';
+        }
+    };
 });
+
