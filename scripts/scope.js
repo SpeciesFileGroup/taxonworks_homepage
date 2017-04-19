3d26170380f2b217b6e1ff421d7c3689c51b51d3
Aaron McNeal <aaron@pixotech.com>
2017-04-19 16:57:30 -0500

    Added scripting and styling to handle scope card status and availability color coding. Started initial scope setup function to hide inactive cards.

--- scripts/scope.js
+++ scripts/scope.js
@@ -1,15 +1,86 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    let featureCardList = document.querySelectorAll('.feature-card');
-    console.log(featureCardList);
+    const cardList = document.querySelectorAll('.feature-card');
 
-    for( i = 0; i < featureCardList.length; i++ )
-    {
-        // let featureCardLabelElement = featureCard.querySelector('.feature-card__status-label').value;
-        // let featuredCardLabelValue = featureCard.querySelector('.feature-card__status-value').value;
-        // console.log(featuredCardLabelValue);
-        console.log('Number of loops: ' + i);
-    }
-
+    changeCardDetailColors(cardList);
+    setupInitialCardState(cardList);
 });
 
+function changeCardDetailColors(cardList) {
+    for( i = 0; i < cardList.length; i++ )
+    {
+        const card = cardList[i];
+        setStatusColor(card);
+        setAvailableColor(card);
+    }
+}
+
+function setStatusColor(card) {
+    const cardLabelElement = card.querySelector('.feature-card__status-value');
+
+    addStatusColorClass(cardLabelElement);
+
+}
+
+function addStatusColorClass(cardLabelElement) {
+    const cardLabelValue = cardLabelElement.innerHTML;
+
+    if(cardLabelValue == 'Upcoming')
+    {
+        cardLabelElement.className += ' status-upcoming';
+    }
+    else if(cardLabelValue == 'In Progress')
+    {
+        cardLabelElement.className += ' status-in-progress';
+    }
+    else if(cardLabelValue == 'Complete')
+    {
+        cardLabelElement.className += ' status-complete';
+    }
+
+}
+
+function setAvailableColor(card) {
+    const cardLabelElement = card.querySelector('.feature-card__available-value');
+
+    addAvailableColorClass(cardLabelElement);
+}
+
+function addAvailableColorClass(cardLabelElement) {
+    const cardLabelValue = cardLabelElement.innerHTML;
+
+    if(cardLabelValue == 'Within the next three years' || cardLabelValue == 'Next year')
+    {
+        cardLabelElement.className += ' available-next-or-three-years';
+    }
+    else if(cardLabelValue == 'This year')
+    {
+        cardLabelElement.className += ' available-this-year';
+    }
+    else if(cardLabelValue == 'Now')
+    {
+        cardLabelElement.className += ' available-now';
+    }
+}
+
+function setupInitialCardState(cardList) {
+    for( i = 0; i < cardList.length; i++ )
+    {
+        if(i == 0)
+        {
+            continue;
+        }
+        setCardAsInactive(cardList[i]);
+    }
+
+}
+
+function setCardAsInactive(card) {
+    card.classList.add('inactive-card');
+
+}
+
+function setCardAsActive() {
+    card.classList.remove('inactive-card');
+
+}
