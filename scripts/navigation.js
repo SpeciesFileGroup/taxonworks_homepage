32165984d8744ec387e7b154799b5138994f2f26
Aaron McNeal <aaron@pixotech.com>
2017-05-12 11:44:46 -0500

    Refactored helper functions to global navigation scope for reusability. Collapsed the mobile links if header bar is moved off-screen.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const twMobileButton = document.querySelector('.header__nav-bar__button');
     const twMobileLinks  = document.querySelector('.header__nav-bar__links__mobile');
+    const twMobileLink   = document.querySelector('.header__nav-bar__links__mobile__link');
 
     window.onscroll = function() {
         const scrollPosition = document.getElementsByTagName("body")[0].scrollTop;
@@ -22,69 +23,82 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             var hideNavBar = anime({
                 targets: twNavBar,
-                translateY: -(twNavBar.offsetHeight)
+                translateY: -(twNavBar.offsetHeight),
+                begin: function() {
+                    getCollapsingAnimation();
+                }
             });   
         }
     };
 
     twMobileButton.addEventListener('click', function() {
         const isCollapsed = checkIfCollapsed(twMobileLinks); 
-        
         this.disabled = true;
 
         if( isCollapsed ){
-            removeCollapsedClass();
-            var showMobileLinks = anime({
-                targets: twMobileLinks,
-                translateY: twMobileLinks.offsetHeight,
-                complete: function() {
-                    twMobileButton.disabled = false;
-                    changeButtonHTML(twMobileButton, isCollapsed);
-                }
-            });
+            getExpandingAnimation();
         }
         else {
-            var hideMobileLinks = anime({
-                targets: twMobileLinks,
-                translateY: 0,
-                complete: function() {
-                    twMobileButton.disabled = false;
-                    changeButtonHTML(twMobileButton, isCollapsed);
-                    addCollapsedClass();
-                }
-            });
-            
-        }
-
-        function checkIfCollapsed(element) {
-            const currentObjectDisplay = getCurrentCollapsedClassState(element);
-
-            return currentObjectDisplay;
-        }
-
-        function getCurrentCollapsedClassState(element) {
-            if ( element.classList.contains(className) )
-                return true;
-            else
-                return false;
-        }
-
-        function addCollapsedClass() {
-            twMobileLinks.classList.add(className);
-        }
-
-        function removeCollapsedClass() {
-            twMobileLinks.classList.remove(className);
-        }
-
-        function changeButtonHTML(button, isCollapsed) {
-            if( isCollapsed )
-                button.innerHTML = 'X';
-            else
-                button.innerHTML = '&#9776;';
+            getCollapsingAnimation();
         }
 
     });
+
+    twMobileLink.addEventListener('click', function() {
+        console.log('You clicked a link');
+        getCollapsingAnimation();
+    });
+
+    function checkIfCollapsed() {
+        const currentObjectDisplay = getCurrentCollapsedClassState(twMobileLinks);
+        return currentObjectDisplay;
+    }
+
+    function getCurrentCollapsedClassState(element) {
+        if ( element.classList.contains(className) )
+            return true;
+        else
+            return false;
+    }
+
+    function changeButtonHTML(button, isCollapsed) {
+        if( isCollapsed )
+            button.innerHTML = '&#9776;';
+        else
+            button.innerHTML = 'X';
+    }
+    
+    function addCollapsedClass() {
+        twMobileLinks.classList.add(className);
+    }
+
+    function removeCollapsedClass() {
+        twMobileLinks.classList.remove(className);
+    }
+
+    function getCollapsingAnimation() {
+        return anime({
+            targets: twMobileLinks,
+            translateY: 0,
+            complete: function() {
+                twMobileButton.disabled = false;
+                addCollapsedClass();
+                changeButtonHTML(twMobileButton, checkIfCollapsed());
+            }
+        });
+    }
+
+    function getExpandingAnimation() {
+        removeCollapsedClass();
+        return anime({
+            targets: twMobileLinks,
+            translateY: twMobileLinks.offsetHeight,
+            complete: function() {
+                twMobileButton.disabled = false;
+                changeButtonHTML(twMobileButton, checkIfCollapsed());
+            }
+        });
+    }
 
 });
 
