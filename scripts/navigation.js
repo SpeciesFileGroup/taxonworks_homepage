c2cf6b5304e08d4d5b7ba611c4c23105ddf2e084
Aaron McNeal <aaron@pixotech.com>
2017-05-11 21:43:28 -0500

    Added animations and logic to navigation bear at small widths.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,12 +1,15 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    var twHeader = document.querySelector('header');
-    var twNavBar = document.querySelector('.header__nav-bar');
-    var twMain   = document.querySelector('main');
+    const twHeader = document.querySelector('header');
+    const twNavBar = document.querySelector('.header__nav-bar');
+    const twMain   = document.querySelector('main');
+
+    const twMobileButton = document.querySelector('.header__nav-bar__button');
+    const twMobileLinks  = document.querySelector('.header__nav-bar__links__mobile');
 
     window.onscroll = function() {
-        var scrollPosition = document.getElementsByTagName("body")[0].scrollTop;
-        var windowYPosition = window.scrollY;
+        const scrollPosition = document.getElementsByTagName("body")[0].scrollTop;
+        const windowYPosition = window.scrollY;
 
         if ( scrollPosition > twMain.offsetTop || windowYPosition > twMain.offsetTop )
         {
@@ -21,6 +24,57 @@ document.addEventListener("DOMContentLoaded", function() {
             });   
         }
     };
+
+    twMobileButton.addEventListener('click', function() {
+        const isCollapsed = checkIfCollapsed(twMobileLinks);
+        this.disabled = true;
+
+        if( isCollapsed ){
+            twMobileLinks.style.display = "block";
+            var showMobileLinks = anime({
+                targets: twMobileLinks,
+                translateY: twMobileLinks.offsetHeight,
+                complete: function() {
+                    twMobileButton.disabled = false;
+                    changeButtonHTML(twMobileButton, isCollapsed);
+                }
+            });
+        }
+        else {
+            var hideMobileLinks = anime({
+                targets: twMobileLinks,
+                translateY: 0,
+                complete: function() {
+                    twMobileButton.disabled = false;
+                    twMobileLinks.style.display = "none";
+                    changeButtonHTML(twMobileButton, isCollapsed);
+                }
+            });
+            
+        }
+
+        function checkIfCollapsed(object) {
+            var currentObjectDisplay;
+
+            if ( object.currentStyle )
+                currentObjectDisplay = object.currentStyle.display;
+            else
+                currentObjectDisplay = getComputedStyle(object, null).display;
+
+            if( object.style.display == "none" || currentObjectDisplay == "none" )
+                return true;
+            else
+                return false;
+        }
+
+        function changeButtonHTML(button, isCollapsed){
+            if( isCollapsed )
+                button.innerHTML = 'X';
+            else
+                button.innerHTML = '&#9776;';
+        }
+
+    });
 
 });
 
